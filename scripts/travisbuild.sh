acde02ad7062b3778fad771e2ddac45a0cddd40f
Milad Rastian <eslashmili@gmail.com>
2017-07-19 12:27:05 +0200

    set git config

--- scripts/travisbuild.sh
+++ scripts/travisbuild.sh
@@ -2,6 +2,8 @@
 
 cd ~
 
+git config --global user.email "travis@ci.com"
+git config --global user.name "Travis"
 git clone --depth=1 -b maint/v0.26 https://github.com/libgit2/libgit2.git
 cd libgit2/
 
