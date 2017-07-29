fbec06d0a2a9a89a537a14967cd7dba6cbaeea0c
Milad Rastian <eslashmili@gmail.com>
2017-07-29 11:40:38 +0200

    Handle small int and int with the same function

--- c_src/erldecode_helper.c
+++ c_src/erldecode_helper.c
@@ -27,6 +27,15 @@ int erl_decode_validate_int(const char *req, int *req_index, int * term_size) {
     return 0;
 }
 
+int erl_decode_validate_small_int(const char *req, int *req_index, int * term_size) {
+    int term_type;
+    if (ei_get_type(req, req_index, &term_type, term_size) < 0 ||
+            term_type != ERL_SMALL_INTEGER_EXT) {
+        return -1;
+    }
+    return 0;
+}
+
 
 int erl_decode_binary(const char *req, int *req_index, void *p, long * binary_len) {
     if (ei_decode_binary(req, req_index, p, binary_len) < 0) {
@@ -53,7 +62,9 @@ int erl_validate_and_decode_string(const char *req, int *req_index, char **out, 
 int erl_validate_and_decode_int(const char *req, int *req_index, int * out) {
     long long_out;
     int term_size;
-    if(erl_decode_validate_int(req, req_index, &term_size) < 0) {
+    int is_int = erl_decode_validate_int(req, req_index, &term_size) < 0;
+    int is_small_int = erl_decode_validate_small_int(req, req_index, &term_size) < 0;
+    if(!(is_small_int ^ is_int)) {
         return -1;
     }
 
